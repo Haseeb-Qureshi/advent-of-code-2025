45881caa7d44b475b832ed0ba96d92d245735d8b
Haseeb Qureshi <haseeb@haseebq.com>
2025-12-01 01:24:13 -0500

    Complete day 1

--- day01.rb
+++ day01.rb
@@ -1,15 +1,6 @@
 input = File.readlines('data01.txt').map(&:chomp)
-# input = 'L68
-# L30
-# R48
-# L5
-# R60
-# L55
-# L1
-# L99
-# R14
-# L82'.lines.map(&:chomp)
 turns = input.map { |l| (l[0] == 'L' ? -1 : 1) * l[1..-1].to_i }
+
 zeroes = 0
 state = 50
 
@@ -17,6 +8,30 @@ turns.each do |turn|
     state += turn
     state %= 100
     zeroes += 1 if state == 0
+end
+
+puts zeroes
+
+# Part 2
+
+zeroes = 0
+state = 50
+
+turns.each do |turn|
+    state += turn
+
+    if state <= 0
+        if state == turn # started on zero
+            zeroes += state.abs / 100
+        else
+            zeroes += (state / 100).abs
+        end
+        zeroes += 1 if state % 100 == 0
+    elsif state > 99
+        zeroes += state / 100
+    end
+
+    state %= 100
 end
 
 puts zeroes
