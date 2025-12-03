620ed1ee6a56cabc5de69f6cb7fadda4cb1095f9
Haseeb Qureshi <haseeb@haseebq.com>
2025-12-03 01:12:59 -0500

    Finish part 2

--- day03.rb
+++ day03.rb
@@ -1,18 +1,8 @@
 input = File.readlines('data03.txt').map(&:chomp)
-# input = '987654321111111
-# 811111111111119
-# 234234234234278
-# 818181911112111'.lines.map(&:chomp)
 
 def largest_joltage(line)
   # find the highest digit from the left
-  max = 0
-  max_index = 0
-  
-  line.chars[0..-2].each_with_index do |battery, i|
-    next unless battery.to_i > max
-    max, max_index = battery.to_i, i
-  end
+  max, max_index = line.chars[0..-2].each_with_index.max_by(&:first).map(&:to_i) 
   
   # find highest_digit from the right of that
   second_digit = line[max_index + 1..-1].chars.max_by(&:to_i).to_i
@@ -21,3 +11,13 @@ def largest_joltage(line)
 end
 
 puts input.sum { |line| largest_joltage(line) }
+
+# Part 2
+
+def rest_of_joltage(line, more: 12)
+  return '' if more == 0
+  max, max_index = line.chars[0..-more].each_with_index.max_by(&:first)
+  max + rest_of_joltage(line[max_index + 1..-1], more: more - 1)
+end
+
+puts input.sum { |line| rest_of_joltage(line).to_i }
