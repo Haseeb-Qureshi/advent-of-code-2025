e1a90e102b190305ca55b1691fd3847491af3717
Haseeb Qureshi <haseeb@haseebq.com>
2025-12-07 16:04:51 -0500

    Complete day 7

--- day07.rb
+++ day07.rb
@@ -1,31 +1,12 @@
-require_relative 'helpers'
-
-input = 
-'.......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............'.lines.map(&:chomp)
-input = File.readlines('data07.txt').map(&:chomp)
+input = File.readlines('data07.txt').map(&:chomp).map(&:chars)
 
 start = [1, input[0].index('S')]
-
 queue = [start]
 splits = 0
 
-while queue.any?
-  i, j = queue.pop
+until queue.empty?
+  i, j = queue.shift
+
   loop do
     break if j >= input[0].length
     break if i >= input.length
@@ -33,13 +14,12 @@ while queue.any?
     case input[i][j]
     when '.'
       input[i][j] = '|'
-    when '|'
-      # already visited; do nothing
-      break
     when '^'
       splits += 1
       queue << [i, j + 1]
       queue << [i, j - 1]
+      break
+    else # is a |, do nothing
       break
     end
 
@@ -47,6 +27,40 @@ while queue.any?
   end
 end
 
-
-puts input
 puts splits
+
+# Part 2
+# Go through line by line left-to-right
+# Replace all of the |'s with their total input streams
+
+input[0][input[0].index('S')] = 1 # set the first 1
+
+input.each_with_index do |line, i|
+  next if i == 0
+  line.each_with_index do |c, j|
+    above = input[i - 1][j]
+
+    case c
+    when '|'
+      input[i][j] = above if above.is_a?(Integer)
+    when '^'
+      next unless above.is_a?(Integer)
+
+      [j - 1, j + 1].each do |j2|
+        if input[i][j2] == '|'
+          input[i][j2] = above
+        else # it's a number already
+          input[i][j2] += above
+        end
+      end
+    when Integer
+      input[i][j] += above if above.is_a?(Integer)
+    when '.'
+      # do nothing
+    else 
+      raise 'wtf'
+    end
+  end
+end
+
+puts input.last.select { |el| el.is_a?(Integer) }.sum
