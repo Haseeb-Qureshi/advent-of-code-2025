cf9f4d398df167b29aa4214c652c254e5d559375
Haseeb Qureshi <haseeb@haseebq.com>
2025-12-06 21:10:33 -0500

    Complete day 5

--- day05.rb
+++ day05.rb
@@ -1,14 +1,3 @@
-input = "3-5
-10-14
-16-20
-12-18
-
-1
-5
-8
-11
-17
-32".lines.map(&:chomp)
 input = File.readlines('data05.txt').map(&:chomp)
 
 ranges = []
@@ -24,3 +13,22 @@ until input.empty?
 end
 
 puts ids.count { |id| ranges.any? { |r| r.include?(id) } }
+
+# Part 2
+
+# Compact the ranges to make them contiguous
+
+sorted_ranges = ranges.sort_by(&:begin)
+combined_ranges = [sorted_ranges.shift]
+
+sorted_ranges.each do |r2|
+  r1 = combined_ranges.last
+
+  if r2.begin > r1.end
+    combined_ranges << r2
+  else r1.begin == r2.begin
+    combined_ranges[-1] = Range.new(r1.begin, [r1.end, r2.end].max)
+  end
+end
+
+puts combined_ranges.sum(&:size)
