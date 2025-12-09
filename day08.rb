e28f487257ca7cd847041ea48b83b5358a0064d2
Haseeb Qureshi <haseeb@haseebq.com>
2025-12-08 20:22:39 -0800

    Finish day 8

--- day08.rb
+++ day08.rb
@@ -1,26 +1,5 @@
-input = '162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689'.lines.map(&:chomp)
 input = File.readlines('data08.txt').map(&:chomp)
 
-# create edges & circuits
 Box = Struct.new(:x, :y, :z)
 class Box
   def dist_to(box2)
@@ -28,10 +7,7 @@ class Box
   end
 end
 
-boxes = input.map do |l|
-  Box.new(*l.split(',').map(&:to_i))
-end
-
+boxes = input.map { |l| Box.new(*l.split(',').map(&:to_i)) }
 distances = []
 boxes.combination(2).each do |b1, b2|
   distances << [b1, b2, b1.dist_to(b2)]  
@@ -39,38 +15,37 @@ end
 
 distances.sort_by!(&:last)
 
-connections = Hash.new { |h, k| h[k] = Set.new }
-CLOSEST_CONNECTIONS = 1000
+class Circuit < Set; end
 
-# Find ten closest connections
+circuits = Hash.new { |h, k| h[k] = Circuit.new }
+
+CLOSEST_CONNECTIONS = 1000
 CLOSEST_CONNECTIONS.times do 
   b1, b2, _ = distances.shift
-  connections[b1] << b2
-  connections[b2] << b1
+  circuit = Circuit.new([b1, b2]) | circuits[b1] | circuits[b2]
+  circuits[b1].each { |b| circuits[b] = circuit }
+  circuits[b2].each { |b| circuits[b] = circuit }
+
+  circuits[b1] = circuit
+  circuits[b2] = circuit
 end
 
-def connected_components(graph, connections)
-  seen = Set.new
-  circuits = []
+puts circuits.values.uniq.map(&:size).sort.last(3).reduce(:*)
 
-  graph.each do |b|
-    next if seen.include?(b)
-    circuits << dfs!(b, seen, connections)
-  end
-  circuits
+# Part 2
+
+last_two_boxes = []
+biggest_circuit = circuits.values.max_by(&:size)
+until biggest_circuit.size == boxes.count
+  b1, b2, _ = distances.shift
+  last_two_boxes = [b1, b2]
+  circuit = Circuit.new([b1, b2]) | circuits[b1] | circuits[b2]
+  circuits[b1].each { |b| circuits[b] = circuit }
+  circuits[b2].each { |b| circuits[b] = circuit }
+
+  circuits[b1] = circuit
+  circuits[b2] = circuit
+  biggest_circuit = [biggest_circuit, circuit].max_by(&:size)
 end
 
-def dfs!(b, seen, connections)
-  return [] if seen.include?(b)
-  seen << b
-
-  circuit = [b]
-  connections[b].each do |b2|
-    circuit += dfs!(b2, seen, connections)
-  end
-  circuit
-end
-
-require 'pry-byebug'
-circuits = connected_components(boxes, connections)
-puts circuits.map(&:length).sort.last(3).reduce(:*)
+p last_two_boxes.map(&:x).reduce(:*)
